2a92a3d140d23c0f668b783287276c4d2d7a19b0
anasofiagif <48517703+anasofiagif@users.noreply.github.com>
2022-02-24 15:42:12 +0000

    Added file server

---
+++ TP2/fileServer.js
@@ -0,0 +1,27 @@
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    var myurl = req.url.substring(1)
+
+    console.log(myurl)
+
+    fs.readFile('./' + myurl, function(err, data){
+
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+
+        if(err){
+            res.write("<p>Erro na leitura do ficheiro...</p>")
+        }
+        else{
+            res.write(data)
+        }
+        res.end()
+    })
+
+}).listen(1441)
+
+console.log("Server listening on port 1441...")
+
